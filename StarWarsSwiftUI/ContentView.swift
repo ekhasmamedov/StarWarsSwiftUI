194fa7a8505f6b9d9658d72bf5df952e3df32718
Eldar Khasmamedov <ekhasm@gmail.com>
2020-09-08 16:14:59 -0400

    Initial Commit

---
+++ StarWarsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarWarsSwiftUI
+//
+//  Created by Eldar Khasmamedov on 2020-09-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarWarsSwiftUI/StarWarsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsSwiftUIApp.swift
+//  StarWarsSwiftUI
+//
+//  Created by Eldar Khasmamedov on 2020-09-08.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
